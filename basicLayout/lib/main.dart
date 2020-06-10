a09685958c6083f177e52aa5643f8f31c0ed46ee
Tiago Mesquita <tiagomac@gmail.com>
2020-06-09 23:47:24 -0300

    new adjustments in layout

--- basicLayout/lib/main.dart
+++ basicLayout/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -6,6 +8,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Color(0xff009688),
+        accentColor: Color(0xff7C4DFF),
+        textTheme: TextTheme(
+          bodyText2: TextStyle(
+            fontSize: 24,
+            fontStyle: FontStyle.italic,
+          )
+        )
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Building Layouts with Flutter'),
@@ -15,6 +28,26 @@ class MyApp extends StatelessWidget {
             style: TextStyle(fontSize: 24),
           ),
       ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.lightbulb_outline),
+          onPressed: (){
+            print('You rang?');
+          },
+        ),
+        persistentFooterButtons: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add_comment),
+              onPressed: (){},
+          ),
+          IconButton(
+            icon: Icon(Icons.add_alarm),
+              onPressed: (){},
+          ),
+          IconButton(
+            icon: Icon(Icons.add_location),
+              onPressed: (){},
+          ),
+        ],
       )
     );
   }
